ee008df31e9a854a8f41ed27e80d4b4281e126a1
Richmond Watkins <richmondwatkins@mac.com>
2014-07-22 12:11:18 -0500

    pathlength

--- lib/distance.rb
+++ lib/distance.rb
@@ -1,15 +1,17 @@
 require 'matrix'
-class Distance
-Vector = Matrix::Vector
 
-  def calculate(vector1, vector2)
-    vector_1 = vector1.to_a
-    vector_2 = vector2.to_a
+class Distance < Vector
 
-    distance = Math.sqrt((vector_1[1] - vector_1[0])**2 + (vector_2[1] - vector_2[0])**2)
-
-
+  def distance(v)
+     Math.sqrt((self[0] - v[0])**2 + (self[1] - v[1])**2)
   end
 
-
+  def self.pathlength(*n) #class method because it starts with self
+    total_distance = 0
+    n.each_index do |p1, p2|
+      total_distance += n[p1].distance(p2) if p1 && p2
+      return total_distance
+    end
+    total_distance
+  end
 end
